3541c69fcec150ca80ad85070521cc2bd6f40f65
yul-mlk <yul.mlk.jd@gmail.com>
2022-12-14 01:54:49 +0200

    refactor

--- comYuliia/src/homework5/courseJavaDeveloper/lectures/LectureService.java
+++ comYuliia/src/homework5/courseJavaDeveloper/lectures/LectureService.java
@@ -5,18 +5,15 @@ class LectureService {
         Lecture lecture = new Lecture();
         Lecture lecture2 = new Lecture();
         Lecture lecture3 = new Lecture();
-        Lecture lecture4 = new Lecture();
 
         lecture2.homeWork = "Create two methods";
         lecture3.lectureName = "Methods";
-        lecture4.addMat = "Link";
 
         System.out.println("Total lectures created:" + Lecture.lectureCounter);
         System.out.println();
         System.out.println("Name of the lecture: " + lecture.lectureName);
         System.out.println("Name of the lecture: " + lecture3.lectureName);
         System.out.println("Your home work: " + lecture2.homeWork);
-        System.out.println("Your additional materials: " + lecture4.addMat);
 
 
     }
