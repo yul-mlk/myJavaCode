291e3cfc5a0c4737d1a156674fd75d33a4c49b43
yul-mlk <yul.mlk.jd@gmail.com>
2023-01-21 16:59:50 +0200

    commit homework17

---
+++ comYuliia/src/homework17/RESOURCE_TYPE.java
@@ -0,0 +1,11 @@
+package homework17;
+
+public enum RESOURCE_TYPE {
+    URL,
+    VIDEO,
+    BOOK;
+
+    RESOURCE_TYPE() {
+    }
+}
+
---
+++ comYuliia/src/homework17/Teacher.java
@@ -0,0 +1,20 @@
+package homework17;
+
+public class Teacher {
+    Integer id;
+    String name;
+
+    public Teacher(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
